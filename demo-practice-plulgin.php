cfc0c12c91bb19418fc091f544f6d9d73031c4b1
Zakaria Ahmed <jakaria9404@gmail.com>
2024-02-04 00:58:16 +0600

    create sample demo plugin with main branch by first lesson

---
+++ demo-practice-plulgin.php
@@ -0,0 +1,11 @@
+<?php
+/** 
+* Plugin Name: Demo Practice Plugin 
+* PLugin Uri: https://wordpress.org
+* Author: Md Zakaria Ahmed
+* Author uri: https://demo.welabs
+* Version: 0.0.1
+* Description: Demo plugin for practise Everyday
+* Lisence: GPL V2
+* Text Domain: demopracticeplugin
+*/
