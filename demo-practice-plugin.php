563fa8159bbe15231a2199c95473ea2e1513b65a
Zakaria Ahmed <jakaria9404@gmail.com>
2024-02-05 06:41:38 +0600

    how to create shortcode

--- demo-practice-plugin.php
+++ demo-practice-plugin.php
@@ -15,3 +15,24 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+// register_activation_hook(__FILE__, 'demo_register_activation_hook');
+// register_deactivation_hook(__FILE__, 'demo_register_deactivation_hook');
+// register_uninstall_hook( __FILE__, 'demo_register_uninstall_hook' );
+
+// function demo_register_activate_hook(){
+//     delete_option('updateTitle','demo plugin is now activate');
+// }
+// function demo_register_deactivate_hook(){
+    
+// }
+
+add_action("init", "demo_short_code"); 
+//(string $hook_name, callable $callback, int $priority = 10, int $accepted_args = 1)
+
+function demo_short_code(){
+    add_shortcode("myshortcode","shortcodefun");
+}
+
+function shortcodefun($attr){
+    return "<strong>demo shortcode is now working</strong>";
+}
